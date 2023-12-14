34a6930b6aa09499b0d16aa1a69aa16a3e054031
lucas Pedrolo <lucassamuelpedrolo@gmail.com>
2023-12-13 22:51:11 -0300

    primeiros usos do useeffect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 //aqui aonde você pode criar o site de uma maneira mais eficiente e pratica
 
 function App() {
 
-  const ApiConection = async () => {
+  const [imagens, setImagens] = useState([])
+
+  useEffect(() => {
     const api = 'https://jsonplaceholder.typicode.com/photos';
+    let imagens = [];
+    
+    fetch(api).then(response => response.json()).then(json => json.map(value => {
+      console.log(value, json.value)
+      if(json.value[id] < 10){
+        imagens.push(value)
+      } 
+    }));
 
-    fetch(api).then(response => {response.json()}).then(json => console.log(json))
-  }
+}, []);
 
 
-  return (
-    <>
-      <div className=''>
-        <h1>opa</h1>
-      </div>
-    </>
-  )
-}
+return (
+  <>
+    {console.log(imagens)}
+    <div>
+      <h1>opa</h1>
+    </div>
+  </>
+)
+};
 
 export default App;
