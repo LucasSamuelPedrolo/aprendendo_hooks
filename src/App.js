45a9663421b3014ce379ad8906af4dfd959367a2
lucas Pedrolo <lucassamuelpedrolo@gmail.com>
2023-12-18 19:59:51 -0300

    testando props

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import './App.css';
 
 //aqui aonde você pode criar o site de uma maneira mais eficiente e pratica
 
+const clickElement = (props) => {
+  console.log(props.id)
+}
+
 function App() {
 
   const [imagens, setImagens] = useState([]);
@@ -26,23 +30,28 @@ function App() {
         setImagens(maxQuantityImage);
       });
 
-    console.log(loading);
   }, []);
 
   return (
     <div className='content'>
       <h1 className='title'>opa</h1>
+      <div className='imageFocus'>
+        <button type='button' className='buttonNext' id='left'>&lt;</button>
+        <div className='imageFocusContent'></div>
+        <button type='button' className='close'>X</button>
+        <button type='button' className='buttonNext' id='right'>&gt;</button>
+      </div>
       <div className='groupImage'>
         {loading && imagens.map((img) => (
-          <div className='imageContent' key={img.id}>
+          <div className='imageContent' key={img.id} props={img} onClick={clickElement}>
             <img src={img.url} alt={img.title}></img>
             <h2>{img.title}</h2>
             <p>Texto ficticio de teste para a imagem de nº {img.id}</p>
           </div>
         ))}
 
-        {loading === false < 0 &&
-          <h2>Ocorreu um erro</h2>
+        {(loading === false) < 0 &&
+          <h2>Carregando imagens...</h2>
         }
       </div>
     </div>
