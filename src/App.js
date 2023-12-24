8f572c2bb43d9c77fe352a0cbab2e1051542d16c
lucas Pedrolo <lucassamuelpedrolo@gmail.com>
2023-12-23 21:44:23 -0300

    att jsx

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
+import { PostImage } from './components/PostImage';
 
 
 //aqui aonde você pode criar o site de uma maneira mais eficiente e pratica
@@ -41,18 +42,12 @@ function App() {
         <button type='button' className='close'>X</button>
         <button type='button' className='buttonNext' id='right'>&gt;</button>
       </div>
-      <div className='groupImage'>
-        {loading && imagens.map((img) => (
-          <div className='imageContent' key={img.id} props={img} onClick={clickElement}>
-            <img src={img.url} alt={img.title}></img>
-            <h2>{img.title}</h2>
-            <p>Texto ficticio de teste para a imagem de nº {img.id}</p>
-          </div>
-        ))}
 
-        {(loading === false) < 0 &&
-          <h2>Carregando imagens...</h2>
-        }
+      <div className='groupImage'>
+        <PostImage
+          loadingConfirmation={loading}
+          imagensList={imagens}
+        />
       </div>
     </div>
   );
---
+++ src/components/PostImage/index.jsx
@@ -0,0 +1,26 @@
+export const PostImage = ({ loadingConfirmation, imagensList }) => {
+
+  console.log(loadingConfirmation, imagensList)
+
+  const loading = loadingConfirmation;
+  const imagens = imagensList;
+
+  return (
+    <>
+      {loading && imagens.map((img) => (
+        <div className='imageContent' key={img.id} props={img}>
+          <img src={img.url} alt={img.title}></img>
+          <h2>{img.title}</h2>
+          <p>Texto ficticio de teste para a imagem de nº {img.id}</p>
+        </div>
+      ))}
+
+      {
+        (loading === false) < 0 &&
+        <h2>Carregando imagens...</h2>
+      }
+    </>
+  )
+}
+
+
